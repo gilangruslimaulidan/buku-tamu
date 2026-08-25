b60f4a83622b66123ead6536211a4cbf0741485c
Gilang Rusli Maulidan <gmaulidan0878@gmail.com>
2026-08-25 17:21:23 +0700

    menambahkan halaman buku tamu dan fitur menampilkan datatamu

--- templates/header.php
+++ templates/header.php
@@ -20,6 +20,9 @@
 <!-- Custom styles for this template-->
 <link href="assets/css/sb-admin-2.min.css" rel="stylesheet">
 
+<!-- Custom styles for this page -->
+<link href="assets/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+
 </head>
 
 <body id="page-top">
@@ -131,6 +134,7 @@
                                 </form>
                             </div>
                         </li>
+
 
                         <div class="topbar-divider d-none d-sm-block"></div>
 
